1a37541cb98d94e05b5277af068ca859ec0ddc4d
Miriam Sánchez <miriamaliciasanchezcervantes@gmail.com>
2024-09-11 23:22:56 -0600

    Cambios de practica imagen, boton y textfield

--- app/src/main/java/com/example/dogyears/MainActivity.kt
+++ app/src/main/java/com/example/dogyears/MainActivity.kt
@@ -4,44 +4,137 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dogyears.ui.theme.DogYearsTheme
 
+val LightPink = Color(0xFFFFC0CB)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DogYearsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun GreetingImage() {
+    val imagen = painterResource(id = R.drawable.perrito)
+    Box(
+        modifier = Modifier
+            .size(400.dp, 200.dp)
+
+
+
+    ){
+        Image(
+            painter = imagen,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(
+                    400.dp,
+                    150.dp
+                )
+
+        )
+
+    }
+}
+
+@Composable
+fun SimpleTextField(){
+    var text by remember{ mutableStateOf(TextFieldValue(""))}
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+
     )
+    {
+        OutlinedTextField(
+            value = text,
+            label = {Text(text = "Enter the age")},
+            onValueChange = { text = it
+
+            },
+            modifier = Modifier
+                .size(
+                    200.dp,
+                    50.dp
+                )
+            
+        )
+
+    }
+}
+
+@Composable
+fun CustomButton(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom,
+
+    ) {
+        Button(
+            onClick = {
+
+            }, shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = LightPink
+
+            ),
+
+        ) {
+            Text(text = "Presioname")
+
+        }
+        Spacer(modifier = Modifier.height(200.dp))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DogYearsTheme {
-        Greeting("Android")
+        GreetingImage()
+        SimpleTextField()
+        CustomButton()
     }
 }
